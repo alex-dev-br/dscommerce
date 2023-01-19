d8d85375aceadaa2fb4a18b268df1c7ab405ca7f
Alexandre Andrade <alex_dev@outlook.com.br>
2023-01-19 11:01:31 -0300

    Database seeding

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -14,7 +14,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(unique = true)
+    private String email;
     private String phone;
+
     private LocalDate birthDate;
     private String password;
 
@@ -23,9 +27,10 @@ public class User {
 
 //    private String roles;
 
-    public User(Long id, String name, String phone, LocalDate birthDate, String password) {
+    public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
         this.name = name;
+        this.email = email;
         this.phone = phone;
         this.birthDate = birthDate;
         this.password = password;
@@ -46,6 +51,14 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPhone() {
